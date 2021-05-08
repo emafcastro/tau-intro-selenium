54c114326a2527c77d1541fc82b45efa7333c52d
emafcastro <castrofemanuel@gmail.com>
2021-05-08 16:46:27 -0300

    Configuration for different browsers

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,38 @@
 This module contains shared fixtures.
 """
 
+import json
 import pytest
 import selenium.webdriver
 
+@pytest.fixture
+def config(scope='session'):
+
+  #Read the file
+  with open('config.json') as config_file:
+    config = json.load(config_file)
+
+  #Assert values are acceptable
+  assert config['browser'] in ['Chrome', 'Headless Chrome']
+  assert isinstance(config['implicit_wait'], int)
+  assert config['implicit_wait'] > 0
+
+  #Return config so it can be used
+  return config
+
 
 @pytest.fixture
-def browser():
+def browser(config):
 
   # Initialize the ChromeDriver instance
-  b = selenium.webdriver.Chrome(executable_path=r"D:\Documents\drivers\chromedriver.exe")
+  if config['browser'] == 'Chrome':
+    b = selenium.webdriver.Chrome(executable_path=r"D:\Documents\drivers\chromedriver.exe")
+  if config['browser'] == 'Headless Chrome':
+    opts = selenium.webdriver.ChromeOptions()
+    opts.add_argument('headless')
+    b = selenium.webdriver.Chrome(executable_path=r"D:\Documents\drivers\chromedriver.exe", options=opts)
+  else:
+    raise Exception(f'Browser "{config["browser"]}" is not supported.')
 
   # Make its calls wait up to 10 seconds for elements to appear
   b.implicitly_wait(10)
--- tests/test_search.py
+++ tests/test_search.py
@@ -10,6 +10,7 @@ def test_basic_duckduckgo_searh(browser):
     search_page = DuckDuckGoSearchPage(browser)
     result_page = DuckDuckGoResultPage(browser)
     PHRASE = "panda"
+    amount_errors = 0
     
     #Given the DuckDuckGo home page is displayed
     search_page.load()
@@ -18,11 +19,19 @@ def test_basic_duckduckgo_searh(browser):
     search_page.search(PHRASE)
 
     #Then the search result title contains "panda"
-    assert PHRASE in result_page.title
+    assert PHRASE in result_page.title()
 
     #And the search result query is "panda"
-    assert PHRASE == result_page.search_input_value
+    assert PHRASE == result_page.search_input_value()
 
-    # And the search result links pertain to "panda"
+    print(*result_page.result_link_titles())
     for title in result_page.result_link_titles():
         assert PHRASE.lower() in title.lower()
+
+    # And the search result links pertain to "panda"
+    #for title in result_page.result_link_titles():
+    #    try:
+    #        assert PHRASE.lower() in title.lower()
+    #    except AssertionError:
+    #        amount_errors += 1
+    #        continue

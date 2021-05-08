b36b77d407c25da37ee35cf993f15aacd6bc897d
emafcastro <castrofemanuel@gmail.com>
2021-05-08 15:38:58 -0300

    Python automation project - First commit

---
+++ pages/search.py
@@ -0,0 +1,28 @@
+"""
+This module contains DuckDuckGoSearchPage,
+the page object for DuckDuckGo search page
+"""
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+class DuckDuckGoSearchPage:
+
+    #URL
+    URL = "https://www.duckduckgo.com"
+
+    SEARCH_INPUT = (By.ID, 'search_form_input_homepage')
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def load(self):
+        self.browser.get(self.URL)
+
+    def search(self, phrase):
+        """the find element method takes two arguments, the locator type and the query
+        but SEARCH_INPUT is a tuple, * is a standar python thing that expand tuples
+        into positional arguments that can be passed to methods
+        """
+        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        search_input.send_keys(phrase + Keys.RETURN)
